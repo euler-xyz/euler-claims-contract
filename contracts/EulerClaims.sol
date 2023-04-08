fa038d547e3006018a9775b7c71d706e8067816c
Doug Hoyte <doug@hcsw.org>
2023-04-08 18:09:23 -0400

    legal text update

--- contracts/EulerClaims.sol
+++ contracts/EulerClaims.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /**********
-By clicking and accepting the redemption, I hereby irrevocably and unconditionally release all claims I (or my company or other separate legal entity) may have against Euler Labs, Ltd., the Euler Foundation, the Euler Decentralized Autonomous Organization, members of the Euler Decentralized Autonomous Organization, and any of their agents, affiliates, officers, employees, or principals related to this matter, whether such claims are known or unknown at this time and regardless of how such claims arise and the laws governing such claims (which shall include but not be limited to any claims arising out of Euler’s terms of use).  This release constitutes an express and voluntary binding waiver and relinquishment to the fullest extent permitted by law.  If I am acting for or on behalf of a company (or other such separate entity), by clicking and accepting the redemption and agreement, I confirm that I am duly authorised to enter into this contract on its behalf.
+By clicking "I Agree" on the euler.finance web interface or executing the EulerClaims smart contract and accepting the redemption, I hereby irrevocably and unconditionally release all claims I (or my company or other separate legal entity) may have against Euler Labs, Ltd., the Euler Foundation, the Euler Decentralized Autonomous Organization, members of the Euler Decentralized Autonomous Organization, and any of their agents, affiliates, officers, employees, or principals related to this matter, whether such claims are known or unknown at this time and regardless of how such claims arise and the laws governing such claims (which shall include but not be limited to any claims arising out of Euler’s terms of use).  This release constitutes an express and voluntary binding waiver and relinquishment to the fullest extent permitted by law.  If I am acting for or on behalf of a company (or other such separate entity), by clicking "I Agree" on the euler.finance web interface or executing the EulerClaims smart contract and accepting the redemption and agreement, I confirm that I am duly authorised to enter into this contract on its behalf.
 
 This agreement and all disputes relating to or arising under this agreement (including the interpretation, validity or enforcement thereof) will be governed by and subject to the laws of England and Wales and the courts of London, England shall have exclusive jurisdiction to determine any such dispute.  To the extent that the terms of this release are inconsistent with any previous agreement and/or Euler’s terms of use, I accept that these terms take priority and, where necessary, replace the previous terms.
 **********/
@@ -16,14 +16,14 @@ contract EulerClaims is ReentrancyGuard {
     //
     // The following is a hash of the above terms and conditions.
     //
-    // By signing and sending a transaction and claiming the redemption tokens, I understand and manifest
-    // my assent and agreement to be bound by the enforceable contract on this page, and agree that all claims
-    // or disputes under this agreement will be resolved exclusively by the courts of London, England in
-    // accordance with the laws of England and Wales. If I am acting for or on behalf of a company (or other
-    // such separate entity), by signing and sending a transaction I confirm that I am duly authorised to
-    // enter into this contract on its behalf.  
+    // By sending a transaction and claiming the redemption tokens, I understand and manifest my assent
+    // and agreement to be bound by the enforceable contract on this page, and agree that all claims or
+    // disputes under this agreement will be resolved exclusively by the courts of London, England in
+    // accordance with the laws of England and Wales. If I am acting for or on behalf of a company (or
+    // other such separate entity), by signing and sending a transaction I confirm that I am duly
+    // authorised to enter into this contract on its behalf.
     //
-    bytes32 constant private termsAndConditionsHash = 0xfff16a3b76758eadffba4d6a838caf02e37303a4fba72441b05aa80bb1474501;
+    bytes32 constant private termsAndConditionsHash = 0x88237f4ba83ebd3a77bb33d44a5067d91fa9dbb17417741c8e224ada10e95314;
 
     string public constant name = "Euler Claims";
 
